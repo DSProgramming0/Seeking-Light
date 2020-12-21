7fffa1d23c9cefe279094a5e82917596c59f6404
DamianoSchirinzi <Damiano16@hotmail.co.uk>
2020-12-21 16:38:32 +0000

    PlayerController, Respawning, Camera and parallax effect added.
    Player can now be controlled.
    If player hits death floors they will be respawned at last checkpoint acquired.
    Camera now follows player.
    Parallax background effect added.

---
+++ Seeking-Light/Assets/Scripts/Managers/Checkpoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    [SerializeField] private RespawnManager _respawnManger;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("detected something");
+
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("PlayerDetected");
+            _respawnManger.setCurrentCheckpoint(this);
+        }
+    }
+}
+
