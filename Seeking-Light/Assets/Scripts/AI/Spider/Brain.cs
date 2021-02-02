1600d0bac6123375b6fb9814ec7d7be383ae51c0
DamianoSchirinzi <Damiano16@hotmail.co.uk>
2021-02-02 20:45:50 +0000

    Added Functionality to spider and added water effect

---
+++ Seeking-Light/Assets/Scripts/AI/Spider/Brain.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brain : MonoBehaviour
+{
+    [Header("IK")]
+    [SerializeField] private Transform IK;
+    [SerializeField] private List<LegStepper> legs;      
+
+    [Header("Movement")]
+    [SerializeField] private Transform target;
+    
+
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] private float moveAcceleration;
+    [SerializeField] private float heightAlterSpeed;
+
+    [SerializeField] private float minDistToTarget;
+    [SerializeField] private float maxDistToTarget;
+
+    Vector3 currentVelocity;
+
+    [SerializeField] LayerMask groundLayerMask;
+
+    [Header("HeightAlteration")]
+    [SerializeField] RaycastHit2D hit0;
+    [SerializeField] RaycastHit2D hit1;
+    [SerializeField] RaycastHit2D hit2;
+    
+    [SerializeField] Transform HeightRay0;
+    [SerializeField] Transform HeightRay1;
+    [SerializeField] Transform HeightRay2;
+
+    [SerializeField] Vector2 heightRayPos0;
+    [SerializeField] Vector2 heightRayPos1;
+    [SerializeField] Vector2 heightRayPos2;
+
+    [SerializeField] float bobbingSpeed;
+    [SerializeField] float rayDist;
+    [SerializeField] float bestHeight;
+    [SerializeField] float yOffset;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        IK.parent = null;
+
+        StartCoroutine(LegUpdateCoroutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rootMotionUpdate();
+        //castHeightRay();
+        RaycastGround();
+    }
+
+    private IEnumerator LegUpdateCoroutine()
+    {
+        while (true)
+        {
+            do
+            {
+                legs[0].TryMove();
+                legs[3].TryMove();
+
+                yield return null;
+
+
+            } while (legs[0].Moving || legs[3].Moving);
+
+            do
+            {
+                legs[1].TryMove();
+                legs[2].TryMove();
+
+                yield return null;
+            } while (legs[1].Moving || legs[2].Moving);
+        }
+    }
+
+    //void castHeightRay()
+    //{
+    //    hit0 = Physics2D.Raycast(HeightRay0.position, -HeightRay0.up, rayDist, groundLayerMask);
+    //    hit1 = Physics2D.Raycast(HeightRay1.position, -HeightRay1.up, rayDist, groundLayerMask);
+    //    hit2 = Physics2D.Raycast(HeightRay2.position, -HeightRay2.up, rayDist, groundLayerMask);
+
+    //    heightRayPos0 = hit0.point;
+    //    heightRayPos1 = hit1.point;
+    //    heightRayPos2 = hit2.point;
+
+    //    float ray0Value = heightRayPos0.y + yOffset;
+    //    float ray1Value = heightRayPos1.y + yOffset;
+    //    float ray2Value = heightRayPos2.y + yOffset;
+
+    //    if(ray0Value > ray1Value && ray0Value > ray2Value)
+    //    {
+    //        bestHeight = ray0Value;
+    //    }
+
+    //    if (ray1Value > ray0Value && ray1Value > ray0Value)
+    //    {
+    //        bestHeight = ray1Value;
+    //    }
+
+    //    if (ray2Value > ray0Value && ray2Value > ray1Value)
+    //    {
+    //        bestHeight = ray2Value;
+    //    }
+
+    //    transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, bestHeight, 1 - Mathf.Exp(-heightAlterSpeed * Time.deltaTime)));
+    //}
+
+    private void RaycastGround()
+    {
+        // int earthLayer = 1 << 8;
+        var startPosition = transform.position;
+        var groundRaycast = Physics2D.Raycast(startPosition, Vector3.down, 15f, groundLayerMask);
+        Debug.DrawRay(startPosition, Vector3.down * 15f, Color.red);
+
+        float pingPongValue = Mathf.PingPong(Time.time * bobbingSpeed, 5f);
+        float newPos = groundRaycast.point.y + yOffset;
+        transform.position = new Vector3(startPosition.x, Mathf.Lerp(startPosition.y, newPos + pingPongValue, 1 - Mathf.Exp(-heightAlterSpeed * Time.deltaTime)));
+        ;
+        // Debug.Log(groundRaycast.distance);
+    }   
+
+    void rootMotionUpdate()
+    {
+        Vector3 towardTarget = target.position - transform.position;
+        Vector3 towardTargetProjected = Vector3.ProjectOnPlane(towardTarget, transform.up);
+        Vector3 targetVelocity = Vector3.zero;
+
+        float distToTarget = Vector3.Distance(transform.position, target.position);
+
+        if(distToTarget > maxDistToTarget)
+        {
+            targetVelocity = moveSpeed * towardTargetProjected.normalized;           
+        }
+        else if(distToTarget < minDistToTarget)
+        {
+            targetVelocity = moveSpeed * -towardTargetProjected.normalized;          
+        }
+        
+        currentVelocity.x = Mathf.Lerp(currentVelocity.x, targetVelocity.x, 1 - Mathf.Exp(-moveAcceleration * Time.deltaTime));
+
+        transform.position = new Vector2(transform.position.x + currentVelocity.x * Time.deltaTime, transform.position.y); 
+
+    }
+
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(HeightRay0.position, Vector2.down * rayDist);
+        Gizmos.DrawRay(HeightRay1.position, Vector2.down * rayDist);
+        Gizmos.DrawRay(HeightRay2.position, Vector2.down * rayDist);
+
+        Gizmos.DrawWireSphere(hit0.point, .15f);
+        Gizmos.DrawWireSphere(hit1.point, .15f);
+        Gizmos.DrawWireSphere(hit2.point, .15f);
+    }
+
+}
---
+++ Seeking-Light/Assets/Scripts/AI/Spider/LegStepper.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegStepper : MonoBehaviour
+{
+    [SerializeField] Transform homeTransform;
+    [SerializeField] Transform target;
+
+    [SerializeField] float rayYoffset;
+    [SerializeField] float heightOffset;
+
+    [SerializeField] float stepOverShootFraction;
+    [SerializeField] float stepAtDistance;
+    [SerializeField] float stepAtAngle = 135f;
+
+    [SerializeField] float moveDuration;
+
+    [SerializeField] LayerMask groundLayerMask;
+    RaycastHit2D hit;
+    
+    public bool Moving { get; private set; }
+    Coroutine moveCoroutine;
+
+    void Awake()
+    {
+        TryMove();
+    }
+
+    public void TryMove()
+    {
+        if (Moving) return;
+
+        float distFromHome = Vector3.Distance(target.position, homeTransform.position);
+        float angleFromHome = Quaternion.Angle(target.rotation, homeTransform.rotation);
+
+        if (distFromHome > stepAtDistance)
+        {
+            if(GetGroundedEndPosition(out Vector3 endPos, out Vector3 endNormal))
+            {
+                Quaternion endRot = Quaternion.LookRotation(Vector3.ProjectOnPlane(homeTransform.forward, endNormal), endNormal);
+                StartCoroutine(MoveToHome(endPos, endRot, moveDuration));
+            }
+        }
+    }
+
+    private bool GetGroundedEndPosition(out Vector3 position, out Vector3 normal)
+    {
+        Vector3 towardHome = (homeTransform.position - transform.position).normalized;
+
+        float overShootDistance = stepAtDistance * stepOverShootFraction;
+        Vector3 overShootVector = towardHome * overShootDistance;
+
+        Vector3 raycastOrigin = homeTransform.position + overShootVector + homeTransform.up * rayYoffset;
+        hit = Physics2D.Raycast(raycastOrigin, -homeTransform.up, Mathf.Infinity, groundLayerMask);
+
+        if(hit.point != null)
+        {
+            position = hit.point;
+            normal = hit.normal;
+            return true;
+        }
+
+        position = Vector3.zero;
+        normal = Vector3.zero;
+        return false;
+    }
+
+    private IEnumerator MoveToHome(Vector3 endPoint, Quaternion endRot, float moveTime)
+    {
+        Moving = true;
+
+        Quaternion startRot = target.rotation;
+        Vector3 startPoint = target.position;
+
+        endPoint += homeTransform.up * heightOffset;
+      
+        Vector3 centerPoint = (startPoint + endPoint) / 2f;
+
+        centerPoint += homeTransform.up * Vector3.Distance(startPoint, endPoint) / 2f;
+
+        float timeElapsed = 0;
+
+        do
+        {
+            timeElapsed += Time.deltaTime;
+
+            float normalizedTime = timeElapsed / moveDuration;
+            normalizedTime = Easing.EaseInOutCubic(normalizedTime);
+
+            target.position = Vector3.Lerp(Vector3.Lerp(startPoint, centerPoint, normalizedTime), Vector3.Lerp(centerPoint, endPoint, normalizedTime), normalizedTime );
+            transform.rotation = Quaternion.Slerp(startRot, endRot, normalizedTime);
+
+            yield return null;
+        }
+        while (timeElapsed < moveDuration);
+
+        Moving = false;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (Moving)
+            Gizmos.color = Color.green;
+        else
+            Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(target.position, 0.25f);
+        Gizmos.DrawLine(target.position, homeTransform.position);
+        Gizmos.DrawWireCube(homeTransform.position, Vector3.one * 0.1f);
+        Gizmos.DrawSphere(hit.point, 0.5f);
+
+    }
+}
